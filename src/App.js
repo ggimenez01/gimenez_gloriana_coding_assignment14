6bff8a91de651360d2f6ac3763cd2ebdf3fd9380
ggimenez01 <126508424+ggimenez01@users.noreply.github.com>
2023-07-30 18:10:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Work from './components/Work';
 import Skills from './components/Skills';
 import DeveloperSetup from './components/DeveloperSetup';
 import SupportingResources from './components/SupportingResources';
-import Education from './components/Education';
+
 import './App.css'; // Import the CSS file for additional styling
 
 function App() {
@@ -18,9 +18,7 @@ function App() {
             <li className="navbar-item">
               <Link to="/" className="navbar-link">Basic Info</Link>
             </li>
-            <li className="navbar-item">
-              <Link to="/education" className="navbar-link">Education</Link>
-            </li>
+           
             <li className="navbar-item">
               <Link to="/work" className="navbar-link">Work</Link>
             </li>
@@ -41,7 +39,7 @@ function App() {
         {/* Routes */}
         <Routes>
           <Route exact path="/" element={<BasicInfo />} />
-          <Route path="/education" element={<Education />} />
+     
           <Route path="/work" element={<Work />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/developer-setup" element={<DeveloperSetup />} />
